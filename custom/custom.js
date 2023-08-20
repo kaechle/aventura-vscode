8cc9d29b67b553daa6115367ff6ccbe1077e4d94
Tim Kaechle <timothy.kaechle@me.com>
2023-08-20 12:31:05 -0300

    syntax highlighting for html

--- custom/custom.js
+++ custom/custom.js
@@ -1,62 +1,58 @@
 setTimeout(function () {
-  document
-    .querySelector('div.monaco-scrollable-element.mac > div.split-view-container')
-    .appendChild(document.getElementById('workbench.parts.statusbar'))
-  document.querySelector('.monaco-workbench .part.basepanel.bottom').id = 'terminal'
-  document.querySelector('#workbench\\.parts\\.titlebar > div > div.titlebar-center > div').id =
-    'window-title'
-  windowTitleContent = document.querySelector(
-    '#workbench\\.view\\.explorer > div > div > div.monaco-scrollable-element.mac > div.split-view-container > div:nth-child(1) > div > div.pane-header.expanded > h3'
-  ).innerHTML
+  window.outerHeight = 1150
+  window.innerHeight = 1150
 
-  // const hardcodedStyles = document.querySelector('.pane-header')
-  // hardcodedStyles.forEach((element) => {
-  //   element.removeAttribute('style')
-  // })
+  document.querySelector('.monaco-workbench .part.basepanel.bottom').id =
+    'terminal'
+  document.querySelector('#terminal> div.content').id = 'panel'
 
-  // const headers =
-  //   sidebar.children[1].children[1].children[0].children[0].children[1].children[0].children
+  document.querySelector(
+    '#workbench\\.parts\\.editor > div.content > div > div > div > div > div.monaco-scrollable-element.mac > div.split-view-container > div > div > div.title.tabs.show-file-icons.title-border-bottom > div.tabs-and-actions-container.tabs-border-bottom > div.monaco-scrollable-element.mac'
+  ).id = 'tabs'
 
-  // const setHeaders = () => {
-  //   const top = 0
-  //   headers.forEach((element) => {
-  //     element.attribute.style = `height: 28px; top: ${top}px;`
-  //     top += 28
-  //   })
-  // }
+  document.querySelector(
+    '#workbench\\.parts\\.editor > div.content > div > div > div > div > div.monaco-scrollable-element.mac > div.split-view-container > div > div > div.title.tabs.show-file-icons.title-border-bottom > div.tabs-breadcrumbs > div > div > div.monaco-breadcrumbs > div:nth-child(2)'
+  ).id = 'breadcrumbs'
 
-  const windowTitle = document.getElementById('window-title')
-  windowTitle.innerHTML = windowTitleContent
-  // windowTitle.append(
-  //   document.querySelector("#status\\.scm\\.0")
-  // )
+  document.querySelector(
+    'body > div.file-icons-enabled.border.enable-motion.monaco-workbench.mac.chromium.macos-bigsur-or-newer.vs-dark.undefined_publisher-aventura-themes-aventura-zen-json.nopanel > div.monaco-grid-view > div > div > div.monaco-scrollable-element.mac > div.split-view-container > div:nth-child(3) > div > div > div.monaco-scrollable-element.mac > div.split-view-container > div:nth-child(3)'
+  ).id = 'editor'
 
+  const tabs = document.getElementById('tabs')
+  const breadcrumbs = document.getElementById('breadcrumbs')
   const sidebar = document.getElementById('workbench.parts.sidebar')
   const auxiliarybar = document.getElementById('workbench.parts.auxiliarybar')
-  const titlebar = document.getElementById('workbench.parts.titlebar')
   const statusbar = document.getElementById('workbench.parts.statusbar')
-  const editor = document.getElementById('workbench.parts.editor')
+  const editor = document.getElementById('editor')
   const terminal = document.getElementById('terminal')
 
   const windowFix = new ResizeObserver((entries) => {
     for (const entry of entries) {
       if (
         entry.target === sidebar ||
-        entry.target === auxiliarybar ||
         entry.target === editor ||
         entry.target === terminal ||
-        entry.target === newActivitybar
+        entry.target === tabs ||
+        entry.target === breadcrumbs
       ) {
-        statusbar.style.marginLeft = window.getComputedStyle(sidebar).width
-        titlebar.style.marginLeft = window.getComputedStyle(sidebar).width
+        statusbar.style.left = window.getComputedStyle(editor).left
         statusbar.style.width = window.getComputedStyle(editor).width
-        titlebar.style.width = statusbar.style.width
-        statusbar.style.marginRight = window.getComputedStyle(auxiliarybar).width
-        titlebar.style.marginRight = statusbar.style.marginRight
+
+        if (tabs == true && window.getComputedStyle(sidebar).width == 'auto') {
+          tabs.style.paddingLeft = '75px'
+        } else if (
+          tabs != true &&
+          window.getComputedStyle(sidebar).width == 'auto'
+        ) {
+          breadcrumbs.style.paddingLeft = '75px'
+        } else {
+          tabs.style.paddingLeft = '0'
+          breadcrumbs.style.paddingLeft = '0'
+        }
 
         if (window.getComputedStyle(terminal).height !== 'auto') {
           statusbar.style.bottom = window.getComputedStyle(terminal).height
-          statusbar.style.marginBottom = '-26px'
+          statusbar.style.marginBottom = '166px'
         } else {
           statusbar.style.bottom = '0'
           statusbar.style.marginBottom = '0'
@@ -66,50 +62,6 @@ setTimeout(function () {
   })
   windowFix.observe(sidebar)
   windowFix.observe(editor)
+  windowFix.observe(tabs)
+  windowFix.observe(breadcrumbs)
 }, 6000)
-
-setTimeout(function () {
-  document.querySelector('#workbench\\.parts\\.sidebar > div.composite.title').innerHTML = ''
-  document.querySelector('#workbench\\.parts\\.sidebar > div.composite.title').id = 'newActivitybar'
-
-  const newActivitybar = document.getElementById('newActivitybar')
-
-  document.querySelector('#anweber\\.statusbar-commands\\.sbc_explorer').id = 'explorer-button'
-  document.querySelector('#anweber\\.statusbar-commands\\.sbc_debug').id = 'debug-button'
-  document.querySelector('#anweber\\.statusbar-commands\\.sbc_extensions').id = 'extensions-button'
-
-  const activityBarExplorerBtn = document.getElementById('explorer-button')
-  const activityBarDebugBtn = document.getElementById('debug-button')
-  const activityBarExtensionsBtn = document.getElementById('extensions-button')
-
-  document.getElementById('workbench.view.debug')
-  document.getElementById('workbench.view.extensions')
-
-  const appendActivityBar = (e) => {
-    newActivitybar.append(e)
-  }
-
-  appendActivityBar(activityBarExplorerBtn)
-  appendActivityBar(activityBarDebugBtn)
-  appendActivityBar(activityBarExtensionsBtn)
-
-  const activeIconColor = (icon) => {
-    const activeColor = '#7c9ddc'
-    const inactiveColor = '#737576'
-    activityBarExplorerBtn.firstChild.firstChild.style.color = inactiveColor
-    activityBarDebugBtn.firstChild.firstChild.style.color = inactiveColor
-    activityBarExtensionsBtn.firstChild.firstChild.style.color = inactiveColor
-
-    if (document.getElementById('workbench.view.explorer')) {
-      activityBarExplorerBtn.firstChild.firstChild.style.color = activeColor
-    } else if (document.getElementById('workbench.view.debug')) {
-      activityBarDebugBtn.firstChild.firstChild.style.color = activeColor
-    } else if (document.getElementById('workbench.view.extensions')) {
-      activityBarExtensionsBtn.firstChild.firstChild.style.color = activeColor
-    }
-  }
-
-  activityBarExplorerBtn.addEventListener('click', activeIconColor, false)
-  activityBarDebugBtn.addEventListener('click', activeIconColor, false)
-  activityBarExtensionsBtn.addEventListener('click', activeIconColor, false)
-}, 7000)
